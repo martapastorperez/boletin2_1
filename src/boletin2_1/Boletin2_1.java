d8e5fdcd738ce601d04899426f3771bc6f00d829
mpastorperez <mpastorperez@atanasoff31.danielcastelao.org>
2016-10-03 09:28:58 +0200

    inicio de boletin

---
+++ src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,20 @@
+
+package boletin2_1;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletin2_1 {
+
+ 
+    public static void main(String[] args) {
+        
+        float base, altura, area;
+        base = Float.parseFloat(JOptionPane.showInputDialog("base"));
+        altura= Float.parseFloat(JOptionPane.showInputDialog("altura"));
+        area= base*altura/2;
+        JOptionPane.showMessageDialog(null, "area= " +area);
+        
+    }
+    
+}
